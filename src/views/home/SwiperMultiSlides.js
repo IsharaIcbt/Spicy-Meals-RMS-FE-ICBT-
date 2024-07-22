aba84215304b668a1bcaa0fd98b96a8772e61e88
IsharaIcbt <isharaicbt@gmail.com>
2024-07-22 23:18:12 +0530

    add SwiperMultiSlides component

--- src/views/home/SwiperMultiSlides.js
+++ src/views/home/SwiperMultiSlides.js
@@ -6,7 +6,7 @@ import { Button, Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap
 import { ArrowRightCircle } from "react-feather"
 import { useNavigate } from "react-router-dom"
 import { CATEGORY_PATH } from "@src/router/routes/route-constant"
-import { categories } from "@src/utility/text_details"
+import { fullDetailsOfSelectCategory } from "@src/utility/text_details";
 
 const params = {
   slidesPerView: 4,
@@ -23,18 +23,22 @@ const SwiperMultiSlides = () => {
   const handleButtonClick = (id) => {
     navigate(`${CATEGORY_PATH}/${id.toLowerCase()}`)
   }
-
+  const cardImgStyles = {
+    width: "100%",
+    height: "300px", // Adjust the height as needed
+    objectFit: "cover" // Ensures the image covers the entire area without distorting
+  }
   return (
     <div className={""}>
       <Swiper {...params}>
-        {categories.map((category, index) => (
+        {fullDetailsOfSelectCategory.map((category, index) => (
           <SwiperSlide key={index}>
             <Card>
-              <CardImg top className="img-fluid" src={category.image} alt={`Card ${index + 1}`} />
+              <CardImg top className="img-fluid" style={cardImgStyles} src={category.image} alt={`Card ${index + 1}`} />
               <CardBody>
-                <CardTitle tag="h4">{category.title}</CardTitle>
-                <CardText>{category.description}</CardText>
-                <Button color="relief-info" onClick={() => handleButtonClick(category.id)}>
+                <CardTitle className={'text-dark fw-bold'}>  {category.title}</CardTitle>
+                <CardText>{category.shortDescription}</CardText>
+                <Button color="relief-warning" onClick={() => handleButtonClick(category.id)}>
                   <span className="fs-5">Find Out More</span> <ArrowRightCircle className="ms-2" size={20} />
                 </Button>
               </CardBody>

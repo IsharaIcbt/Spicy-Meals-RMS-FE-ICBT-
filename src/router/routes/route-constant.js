7b14e61ef12e86724590ce12605bc11a4dbb75e7
IsharaIcbt <isharaicbt@gmail.com>
2024-07-25 22:24:11 +0530

    update routing

--- src/router/routes/route-constant.js
+++ src/router/routes/route-constant.js
@@ -1,7 +1,6 @@
 export const ROUTE_PATH = ''
 
 export const HOME_PATH = `${ROUTE_PATH}/home`
-export const PLACES_PATH = `${ROUTE_PATH}/places`
 export const PLACES_PATH_FILTER = `${ROUTE_PATH}/search-places`
 export const CATEGORIES_PATH = `${ROUTE_PATH}/categories`
 export const SERVICES_PATH = `${ROUTE_PATH}/services`

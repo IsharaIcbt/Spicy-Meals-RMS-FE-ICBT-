b16eb0c5b998ded4f9c19755e8830f6068a163dc
IsharaIcbt <isharaicbt@gmail.com>
2024-07-29 09:04:56 +0530

    content update with redux reducers and store

--- src/redux/rootReducer.js
+++
@@ -1,7 +0,0 @@
-// ** Reducers Imports
-import layout from "./layout"
-import navbar from "./navbar"
-import routePath from "./routePath"
-const rootReducer = { navbar, layout, routePath }
-
-export default rootReducer
--- src/router/routes/route-constant.js
+++ src/router/routes/route-constant.js
@@ -5,4 +5,11 @@ export const PLACES_PATH_FILTER = `${ROUTE_PATH}/search-places`
 export const CATEGORIES_PATH = `${ROUTE_PATH}/categories`
 export const SERVICES_PATH = `${ROUTE_PATH}/services`
 export const CATEGORY_PATH = `${ROUTE_PATH}/category/:categoryTitle`
+export const MENUS_PATH = `${ROUTE_PATH}/menus`
+export const SHOP_PATH = `${ROUTE_PATH}/apps/ecommerce/shop`
+export const MENU_DETAILS_PATH = `${ROUTE_PATH}/menus/:menuId`
+
+export const MEALS_PATH = `${ROUTE_PATH}apps/ecommerce/shop`
+export const SPECIFIC_MEAL_PATH = `${ROUTE_PATH}/meals/:mealId`
+
 export const ADD_NEW_PLACE_PATH = `${ROUTE_PATH}/add-new-place`

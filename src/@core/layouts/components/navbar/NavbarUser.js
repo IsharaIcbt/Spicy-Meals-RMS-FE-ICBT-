137bc12a56126c8780dbd18e8e47ce742be9bae5
IsharaIcbt <isharaicbt@gmail.com>
2024-07-30 23:32:18 +0530

    updated Navbar component

--- src/@core/layouts/components/navbar/NavbarUser.js
+++ src/@core/layouts/components/navbar/NavbarUser.js
@@ -1,11 +1,12 @@
 // ** Dropdowns Imports
-import UserDropdown from "./UserDropdown";
+import UserDropdown from "./UserDropdown"
+import CartDropdown from "@layouts/components/navbar/CartDropdown"
+import React from "react"
 
 const NavbarUser = () => {
-  return (
-    <ul className="nav navbar-nav align-items-center ms-auto">
+  return (<ul className="nav navbar-nav align-items-center ms-auto">
       <UserDropdown />
-    </ul>
-  );
-};
-export default NavbarUser;
+      <CartDropdown />
+    </ul>)
+}
+export default NavbarUser

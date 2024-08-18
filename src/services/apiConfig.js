7582cc0d3bb5f84963d18255e20f42926dd3bf27
IsharaIcbt <isharaicbt@gmail.com>
2024-08-18 14:33:25 +0530

    update port apiConfig 8080

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,7 +1,7 @@
 /* http://13.201.57.37/api/v1/placesWithComments */
 const apiConfig = {
     /*serverUrl: 'http://43.205.216.155',*/
-    serverUrl: 'http://127.0.0.1:8000',
+    serverUrl: 'http://127.0.0.1:8080',
     basePath: 'api/v1',
     port: 80
 }

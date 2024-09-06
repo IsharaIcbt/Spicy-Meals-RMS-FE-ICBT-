07c01905dd15a66aa3d03a9bae9590115a01d81d
IsharaIcbt <isharaicbt@gmail.com>
2024-09-06 11:08:44 +0530

    content updated with NavbarUser component

--- src/@core/layouts/components/navbar/NavbarUser.js
+++ src/@core/layouts/components/navbar/NavbarUser.js
@@ -1,11 +1,22 @@
 // ** Dropdowns Imports
 import UserDropdown from "./UserDropdown"
 import CartDropdown from "@layouts/components/navbar/CartDropdown"
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { IS_LOGIN } from "@src/router/RouteConstant"
 
 const NavbarUser = () => {
-  return (<ul className="nav navbar-nav align-items-center ms-auto">
-      <CartDropdown />
+  const [userStatus, setUserStatus] = useState(localStorage.getItem(IS_LOGIN))
+
+  useEffect(() => {
+    setUserStatus(localStorage.getItem(IS_LOGIN))
+  }, [userStatus])
+  
+  return (
+    <ul className="nav navbar-nav align-items-center ms-auto">
+      <div className={userStatus === 'ADMIN' || userStatus === 'STAFF' ? 'd-none' : ''}>
+        <CartDropdown />
+      </div>
+
       <UserDropdown />
     </ul>)
 }

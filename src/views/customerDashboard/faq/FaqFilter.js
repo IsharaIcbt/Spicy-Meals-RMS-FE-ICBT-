802cdbe8ca2482d5e35341358d53199dfe0caf23
IsharaIcbt <isharaicbt@gmail.com>
2024-09-06 11:43:37 +0530

    create new FaqFilter component

---
+++ src/views/customerDashboard/faq/FaqFilter.js
@@ -0,0 +1,18 @@
+// ** Icons Imports
+// ** Reactstrap Imports
+import { Card, CardBody, CardText } from "reactstrap"
+
+const FaqFilter = () => {
+
+  return (
+    <div id="faq-search-filter">
+      <Card className="faq-search">
+        <CardBody className="text-center">
+          <h2 className="text-primary">You still have a question?</h2>
+          <CardText className="">If you cannot find a question in our FAQ, you can always contact us. We will answer you shortly!</CardText>
+        </CardBody>
+      </Card>
+    </div>)
+}
+
+export default FaqFilter

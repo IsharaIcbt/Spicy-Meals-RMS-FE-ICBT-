e44664538eca7a575582b0c32fa59159a0ff4cbb
IsharaIcbt <isharaicbt@gmail.com>
2024-09-06 11:46:08 +0530

    implement new api services for reservation

---
+++ src/services/reservation.js
@@ -0,0 +1,74 @@
+import ApiService from "@src/services/apiService"
+
+export async function addNewReservation(userDetails) {
+  const apiObject = {}
+  apiObject.method = "POST"
+  apiObject.authentication = true
+  apiObject.endpoint = "reservation/table"
+  apiObject.multipart = false
+  apiObject.urlencoded = false
+  apiObject.body = userDetails
+  return await ApiService.callApi(apiObject)
+}
+
+export async function addNewMealOrder(userDetails) {
+  const apiObject = {}
+  apiObject.method = "POST"
+  apiObject.authentication = true
+  apiObject.endpoint = "reservation/meal"
+  apiObject.multipart = false
+  apiObject.urlencoded = false
+  apiObject.body = userDetails
+  return await ApiService.callApi(apiObject)
+}
+
+
+export async function getAllReservationsForStaff() {
+  const apiObject = {}
+  apiObject.method = "GET"
+  apiObject.authentication = false
+  apiObject.endpoint = `reservation/TABLE`
+  apiObject.multipart = false
+  apiObject.urlencoded = false
+  return await ApiService.callApi(apiObject)
+}
+
+export async function getAllOrdersForStaff() {
+  const apiObject = {}
+  apiObject.method = "GET"
+  apiObject.authentication = false
+  apiObject.endpoint = `reservation/MEAL`
+  apiObject.multipart = false
+  apiObject.urlencoded = false
+  return await ApiService.callApi(apiObject)
+}
+
+export async function submitQueryForReservation(userDetails) {
+  const apiObject = {}
+  apiObject.method = "POST"
+  apiObject.authentication = false
+  apiObject.endpoint = `reservation/query`
+  apiObject.multipart = false
+  apiObject.urlencoded = false
+  apiObject.body = userDetails
+  return await ApiService.callApi(apiObject)
+}
+
+export async function getSpecificQueries(id) {
+  const apiObject = {}
+  apiObject.method = "GET"
+  apiObject.authentication = false
+  apiObject.endpoint = `reservation/TABLE/${id}`
+  apiObject.multipart = false
+  apiObject.urlencoded = false
+  return await ApiService.callApi(apiObject)
+}
+export async function getSpecificQueriesForOrder(id) {
+  const apiObject = {}
+  apiObject.method = "GET"
+  apiObject.authentication = false
+  apiObject.endpoint = `reservation/MEAL/${id}`
+  apiObject.multipart = false
+  apiObject.urlencoded = false
+  return await ApiService.callApi(apiObject)
+}

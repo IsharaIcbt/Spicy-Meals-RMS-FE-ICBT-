22eeeb4f2c1dd525b0d7f2eab5ba603bb3e6cedc
IsharaIcbt <isharaicbt@gmail.com>
2024-09-06 11:10:24 +0530

    implement new auth api services

--- src/services/auth.js
+++ src/services/auth.js
@@ -25,3 +25,37 @@ export async function loginUser(userCredentials) {
     apiObject.type = "AUTH"
     return await ApiService.callApi(apiObject)
 }
+
+export async function forgotPassword(email) {
+    const apiObject = {}
+    apiObject.method = "POST"
+    apiObject.authentication = false
+    apiObject.endpoint = `user/otp/send?email=${email}`
+    apiObject.isBasicAuth = false
+    apiObject.urlencoded = false
+    return await ApiService.callApi(apiObject)
+}
+
+
+export async function sendOTP(userCredentials) {
+    const apiObject = {}
+    apiObject.method = "POST"
+    apiObject.authentication = false
+    apiObject.endpoint = `user/otp/validate`
+    apiObject.body = userCredentials
+    apiObject.isBasicAuth = false
+    apiObject.urlencoded = false
+    return await ApiService.callApi(apiObject)
+}
+
+
+export async function resetPassword(userCredentials) {
+    const apiObject = {}
+    apiObject.method = "PUT"
+    apiObject.authentication = false
+    apiObject.endpoint = `user/password-reset`
+    apiObject.body = userCredentials
+    apiObject.isBasicAuth = false
+    apiObject.urlencoded = false
+    return await ApiService.callApi(apiObject)
+}
